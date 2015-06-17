f2a3d3f5186ac8f9d93116c372bd31d4559d8c6d
ravikasi <ravikasi@LIN36000074.corp.capgemini.com>
2015-06-17 16:27:51 +0530

    commit

---
+++ Assignment2/src/Sum_Digits.java
@@ -0,0 +1,18 @@
+
+
+public class Sum_Digits {
+	public static void main(String[] args) {
+		int number=7890;
+		int rem;
+		int sum=0;
+		while(number!=0)
+		{
+		rem=number%10;
+		sum+=rem;
+		number=number/10;
+		
+		}
+		System.out.println("sum"+sum);
+
+	}
+}
