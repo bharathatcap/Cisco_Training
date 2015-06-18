f11361fe268aee356bff7c3dc1bc150614f26208
ravikasi <ravikasi@LIN36000074.corp.capgemini.com>
2015-06-18 17:36:19 +0530

    commit

---
+++ Assignment4/src/Parking.java
@@ -0,0 +1,17 @@
+
+public class Parking extends KeyValue {
+
+	final int floors=5;
+	KeyValue kl[];
+	public Parking(){
+		kl=new KeyValue[floors];
+		
+		
+		
+}
+	public static void main(String[] args) {
+		
+
+	}
+
+}
